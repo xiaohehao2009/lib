da7aef11620cf7c1d882375d8614a2d8fb5a639e
xiaohehao2009 <1607509493@qq.com>
2024-05-02 13:32:30 +0800

    update html/lifegame

--- html/lifegame/index (1).js
+++ html/lifegame/index (1).js
@@ -11,16 +11,13 @@ try{
         this.hpx = height * squarePx + 1;
         this.el.width = this.wpx;
         this.el.height = this.hpx;
-        this.ctx = this.el.getContext("2d", { alpha: false });
-        this.whites = [];
-        this.blacks = [];
+        this.ctx = this.el.getContext("2d");
         this.initGrid();
+        this.ctx.fillStyle = "#fff";
         document.getElementById("app").appendChild(this.el);
     }
     CanvasOperate.prototype.initGrid = function () {
         const ctx = this.ctx;
-        ctx.fillStyle = "#555";
-        ctx.fillRect(1, 1, width * squarePx, height * squarePx)
         ctx.fillStyle = "#000";
         for (let i = 0; i <= width; i++) {
             ctx.fillRect(i * squarePx, 0, 1, this.hpx);
@@ -30,29 +27,11 @@ try{
         }
     };
     CanvasOperate.prototype.black = function (x, y) {
-        this.blacks.push(x, y);
+        this.ctx.clearRect(x * squarePx + 1, y * squarePx + 1, squarePx - 1, squarePx - 1);
     };
     CanvasOperate.prototype.white = function (x, y) {
-        this.whites.push(x, y);
+        this.ctx.fillRect(x * squarePx + 1, y * squarePx + 1, squarePx - 1, squarePx - 1);
     };
-    CanvasOperate.prototype.done = function () {
-        const w = this.whites;
-        const b = this.blacks;
-        const ctx = this.ctx;
-        ctx.beginPath();
-        for (let i = 0; i < w.length; i += 2) {
-            ctx.rect(w[i] * squarePx + 1, w[i+1] * squarePx + 1, squarePx - 1, squarePx - 1);
-        }
-        ctx.fillStyle = "#fff";
-        ctx.fill();
-        ctx.beginPath();
-        for (let i = 0; i < b.length; i += 2) {
-            ctx.rect(b[i] * squarePx + 1, b[i+1] * squarePx + 1, squarePx - 1, squarePx - 1);
-        }
-        ctx.fillStyle = "#555";
-        ctx.fill();
-        w.length = b.length = 0;
-    }
     CanvasOperate.prototype.draw = function (x, y, flag) {
         flag ? this.white(x, y) : this.black(x, y);
     }
@@ -62,17 +41,13 @@ try{
         // }
         const nx = Math.floor(x / squarePx);
         const ny = Math.floor(y / squarePx);
+        // this.draw(nx, ny, gr.rows[ny][nx] ^= 1);
         gr.invert(nx, ny);
-        this.done();
-        gr.markAround(nx, ny);
-        gr.mark[ny * width + nx] = 1;
+        // alert(gr.key.map(v=>v.join('')).join('\n'))
     };
     function Grid(co) {
         this.rows = this.genRows();
         this.key = this.genRows();
-        // key: 相邻八格内白色块数
-        this.mark = this.genRows();
-        // mark: 是否可以 update
         this.co = co;
     }
     Grid.prototype.genRows = function () {
@@ -90,38 +65,31 @@ try{
                 }
             }
         }
-        co.done();
         rows.fill(0);
     };
     Grid.prototype.update = function () {
         const start = performance.now();
         const co = this.co;
         const key = new Int8Array(this.key);
-        const mark = this.mark;
-        this.mark = this.genRows();
         const rows = this.rows;
         for (let y = 0; y < height; y++) {
             const left = width * y;
             for (let x = 0; x < width; x++) {
-                if (!mark[left + x]) continue;
                 const num = key[left + x];
                 if (rows[left + x]) {
                     if (num !== 2 && num !== 3) {
                         co.black(x, y);
                         rows[left + x] = 0;
                         this.keyDec(x, y);
-                        this.markAround(x, y)
                     }
                 }
                 else if (num === 3) {
                     rows[left + x] = 1;
                     co.white(x, y);
                     this.keyInc(x, y);
-                    this.markAround(x, y);
                 }
             }
         }
-        co.done();
         time.textContent = (performance.now() - start).toFixed(2);
         // alert(key.map(v=>v.join('')).join('\n'))
     };
@@ -165,29 +133,8 @@ try{
             k[left + width + x]--;
         }
     };
-    Grid.prototype.markAround = function (x, y) {
-        const k = this.mark;
-        const left = y * width;
-        if (x !== 0) {
-            k[left + x-1] = 1;
-            if (y !== 0) k[left - width + x-1] = 1;
-            if (y !== height - 1) k[left + width + x-1] = 1;
-        }
-        if (x !== width - 1) {
-            k[left + x+1] = 1;
-            if (y !== height - 1) k[left + width + x+1] = 1;
-            if (y !== 0) k[left - width + x+1] = 1;
-        }
-        if (y !== 0) {
-            k[left - width + x] = 1;
-        }
-        if (y !== height - 1) {
-            k[left + width + x] = 1;
-        }
-    };
     Grid.prototype.invert = function (x, y) {
         this.co.draw(x, y, this.rows[y * width + x] ^= 1);
-        this.markAround(x, y);
         if (this.rows[y * width + x]) {
             this.keyInc(x, y);
         }
@@ -205,7 +152,6 @@ try{
                 }
             }
         }
-        this.co.done();
         time.textContent = (performance.now() - start).toFixed(2);
     }
     const co = new CanvasOperate();
